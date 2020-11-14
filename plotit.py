ac6b892bf760e82a4037cb91187c040dc8642f9a
JakubSekula <kubasekula@seznam.cz>
2020-11-14 23:49:55 +0100

    SEIAHRD

--- plotit.py
+++ plotit.py
@@ -2,14 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-sir_out = pd.read_csv("sir.out",sep=" ",header=None,names=["t","S","I","R"],index_col=False)
+sir_out = pd.read_csv("sir_out",sep=" ",header=None,names=["t","S","E","I","A","H","R","D"],index_col=False)
 
 sline = plt.plot("t","S","",data=sir_out,color="red",linewidth=2)
-iline = plt.plot("t","I","",data=sir_out,color="green",linewidth=2)
+eline = plt.plot("t","E","",data=sir_out,color="green",linewidth=2)
+iline = plt.plot("t","I","",data=sir_out,color="purple",linewidth=2)
+aline = plt.plot("t","A","",data=sir_out,color="pink",linewidth=2)
+hline = plt.plot("t","H","",data=sir_out,color="yellow",linewidth=2)
 rline = plt.plot("t","R","",data=sir_out,color="blue",linewidth=2)
+dline = plt.plot("t","D","",data=sir_out,color="black",linewidth=2)
 plt.xlabel("Time",fontweight="bold")
 plt.ylabel("Number",fontweight="bold")
-legend = plt.legend(title="Population",loc=5,bbox_to_anchor=(1.25,0.5))
+legend = plt.legend(title="Population",loc=10,bbox_to_anchor=(1.25,0.5))
 frame = legend.get_frame()
 frame.set_facecolor("white")
 frame.set_linewidth(0)
--- sir.cpp
+++ sir.cpp
@@ -6,52 +6,55 @@
 using namespace std;
 using namespace boost::numeric::odeint;
 
-double b;
-const double g = 0.05;
+const double b = 0.4;
+const double g = 0.04;
 
-typedef boost::array< double , 3 > state_type;
+const double N = 1000.0;
 
+double eta = 0.5;
+double sigma = 1/5.1;
+double alpha = 0.5;
+double beta = 0.5;
+double greeky = 0.025;
 
+double iotaA = 1/7;
+double iotaI = 1/7;
+double iotaH = 1/14;
 
-void getArgs( int argc, char** argv ){
+double delta = 0.015;
 
-    int arg;
-    string temp = "";
-    while( ( arg = getopt( argc, argv, "r:" ) ) != -1 ){
-        switch( arg ){
-            case 'r':
-                b = stod( optarg );
-                break;
-            default:
-                fprintf( stderr, "Uknown argument\n" );
-                exit( 10 );
-        }
-    }
+typedef boost::array< double , 7 > state_type;
 
-    if( !b ){
-        fprintf( stderr, "Reproduction number must be entered\n" );
-        exit( 10 );
-    }
-
+void masked( const state_type &x , state_type &dxdt , double t )
+{
+    t = t; 
+    dxdt[0] = -b * ( ( x[ 2 ] + eta * x[ 3 ] ) * ( x[ 0 ] / N ) ) ;
+    dxdt[1] = b * ( ( x[ 2 ] + eta * x[ 3 ] ) * ( x[ 0 ] / N ) ) - sigma * x[ 1 ];
+    dxdt[2] = alpha * sigma * x[ 1 ] - greeky * x[ 2 ] - iotaI * x[ 2 ];
+    dxdt[3] = ( 1 - alpha ) * sigma * x[ 1 ] - iotaA * x[ 3 ];
+    dxdt[4] = greeky * x[ 2 ] - delta * x[ 4 ] - iotaH * x[ 4 ];
+    dxdt[5] = iotaI * x[ 2 ] + iotaA * x[ 3 ] + iotaH * x[ 4 ];
+    dxdt[6] = delta * x[ 4 ];
 }
 
 void sir( const state_type &x , state_type &dxdt , double t )
 {
-    t = t; 
-    dxdt[0] = -b * x[0] * x[1];
-    dxdt[1] = b * x[0] * x[1] - g * x[1];
+    t = t;
+    dxdt[0] = ( -b *  x[0] * x[1] ) / N;
+    dxdt[1] = ( b *  ( x[0] * x[1] ) / N )  - g * x[1];
     dxdt[2] = g * x[1];
 }
 
 void write_sir(const state_type &x , const double t )
 {
-    cout << t << ' ' << x[0] << ' ' << x[1] << ' ' << x[2] << endl;
+    cout << t << ' ' << x[0] << ' ' << x[1] << ' ' << x[2] << ' ' << x[3] << ' ' << x[ 4 ] << ' ' << x[ 5 ] << endl;
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    getArgs( argc, argv );
-
-    state_type x = { 0.99 , 0.01 , 0.0 }; // initial conditions
-    integrate( sir , x , 0.0 , 200.0 , 0.1 , write_sir );
+    argc = argc;
+    argv = argv;
+    //                 S     E    I    A    H    R    D
+    state_type x = { N - 6, 2.0, 3.0, 1.0, 0.0, 0.0, 0.0 }; // initial conditions
+    integrate( masked , x , 0.0 , 100.0 , 0.1 , write_sir );
 }
