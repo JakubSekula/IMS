d7dabfc7652b12e9e8ace77215604e582261a7dd
JakubSekula <kubasekula@seznam.cz>
2020-11-15 01:20:24 +0100

    Added completed SEIAHRD model

--- sir.cpp
+++ sir.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace boost::numeric::odeint;
 
-const double b = 0.4;
+const double b = 1.2;
 const double g = 0.04;
 
 double N = 1000.0;
@@ -17,9 +17,9 @@ double alpha = 0.5;
 double beta = 0.5;
 double greeky = 0.025;
 
-double iotaA = 1/7;
-double iotaI = 1/7;
-double iotaH = 1/14;
+double iotaA = 1/7.0;
+double iotaI = 1/7.0;
+double iotaH = 1/14.0;
 
 double delta = 0.015;
 
@@ -35,6 +35,9 @@ void masked( const state_type &x , state_type &dxdt , double t )
     dxdt[4] = greeky * x[ 2 ] - delta * x[ 4 ] - iotaH * x[ 4 ];
     dxdt[5] = iotaI * x[ 2 ] + iotaA * x[ 3 ] + iotaH * x[ 4 ];
     dxdt[6] = delta * x[ 4 ];
+
+    N = x[ 0 ] + x[ 1 ] + x[ 2 ] + x[ 3 ] + x[ 5 ];
+
 }
 
 void sir( const state_type &x , state_type &dxdt , double t )
@@ -43,9 +46,6 @@ void sir( const state_type &x , state_type &dxdt , double t )
     dxdt[0] = ( -b *  x[0] * x[1] ) / N;
     dxdt[1] = ( b *  ( x[0] * x[1] ) / N )  - g * x[1];
     dxdt[2] = g * x[1];
-
-    N = x[ 0 ] + x[ 1 ] + x[ 2 ] + x[ 3 ] + x[ 5 ];
-
 }
 
 void write_sir(const state_type &x , const double t )
@@ -58,6 +58,6 @@ int main(int argc, char **argv)
     argc = argc;
     argv = argv;
     //                 S     E    I    A    H    R    D
-    state_type x = { N - 6, 2.0, 3.0, 1.0, 0.0, 1.0, 0.0 }; // initial conditions
+    state_type x = { N - 6, 2.0, 3.0, 1.0, 0.0, 1.1, 0.0 }; // initial conditions
     integrate( masked , x , 0.0 , 200.0 , 0.1 , write_sir );
 }
