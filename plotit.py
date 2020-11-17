265131bae7b7e3448ae5248885035b29aa006de4
JakubSekula <kubasekula@seznam.cz>
2020-11-17 20:37:06 +0100

    in work

--- plotit.py
+++ plotit.py
@@ -2,15 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
-sir_out = pd.read_csv("sir_out",sep=" ",header=None,names=["t","S","E","I","A","H","R","D"],index_col=False)
+sir_out = pd.read_csv("sir_out",sep=" ",header=None,names=["t","S","E","A","I","Id","R"],index_col=False)
 
 sline = plt.plot("t","S","",data=sir_out,color="red",linewidth=2)
 eline = plt.plot("t","E","",data=sir_out,color="green",linewidth=2)
-iline = plt.plot("t","I","",data=sir_out,color="purple",linewidth=2)
-aline = plt.plot("t","A","",data=sir_out,color="pink",linewidth=2)
-hline = plt.plot("t","H","",data=sir_out,color="yellow",linewidth=2)
+iline = plt.plot("t","A","",data=sir_out,color="purple",linewidth=2)
+aline = plt.plot("t","I","",data=sir_out,color="pink",linewidth=2)
+hline = plt.plot("t","Id","",data=sir_out,color="yellow",linewidth=2)
 rline = plt.plot("t","R","",data=sir_out,color="blue",linewidth=2)
-dline = plt.plot("t","D","",data=sir_out,color="black",linewidth=2)
+
 plt.xlabel("Time",fontweight="bold")
 plt.ylabel("Number",fontweight="bold")
 legend = plt.legend(title="Population",loc=10,bbox_to_anchor=(1.25,0.5))
--- sir.cpp
+++ sir.cpp
@@ -6,39 +6,28 @@
 using namespace std;
 using namespace boost::numeric::odeint;
 
-const double b = 1.2;
+const double b = 0.4236;
 const double g = 0.04;
 
-double N = 1000.0;
+double N = 14368332;
 
-double eta = 0.5;
-double sigma = 1/5.1;
 double alpha = 0.5;
-double beta = 0.5;
-double greeky = 0.025;
+double ny = 0.5;
+double sigma = 1/5.2;
+double theta = 1.8999 * pow( 10, -12 );
+double psi = 0.0135;
 
-double iotaA = 1/7.0;
-double iotaI = 1/7.0;
-double iotaH = 1/14.0;
+double delta = 0.0;
+double epsilon = 0.0;
 
-double delta = 0.015;
+double iotaA = 0.13978;
+double iotaO = 0.13978;
+double iotaI = 1/15;
 
-typedef boost::array< double , 7 > state_type;
+double d0 = 0.1113;
 
-void masked( const state_type &x , state_type &dxdt , double t )
-{
-    t = t; 
-    dxdt[0] = -b * ( ( x[ 2 ] + eta * x[ 3 ] ) * ( x[ 0 ] / N ) ) ;
-    dxdt[1] = b * ( ( x[ 2 ] + eta * x[ 3 ] ) * ( x[ 0 ] / N ) ) - sigma * x[ 1 ];
-    dxdt[2] = alpha * sigma * x[ 1 ] - greeky * x[ 2 ] - iotaI * x[ 2 ];
-    dxdt[3] = ( 1 - alpha ) * sigma * x[ 1 ] - iotaA * x[ 3 ];
-    dxdt[4] = greeky * x[ 2 ] - delta * x[ 4 ] - iotaH * x[ 4 ];
-    dxdt[5] = iotaI * x[ 2 ] + iotaA * x[ 3 ] + iotaH * x[ 4 ];
-    dxdt[6] = delta * x[ 4 ];
-
-    N = x[ 0 ] + x[ 1 ] + x[ 2 ] + x[ 3 ] + x[ 5 ];
-
-}
+typedef boost::array< double , 6 > state_type;
+//typedef boost::array< double , 14 > state_type;
 
 void sir( const state_type &x , state_type &dxdt , double t )
 {
@@ -50,14 +39,29 @@ void sir( const state_type &x , state_type &dxdt , double t )
 
 void write_sir(const state_type &x , const double t )
 {
-    cout << t << ' ' << x[0] << ' ' << x[1] << ' ' << x[2] << ' ' << x[3] << ' ' << x[ 4 ] << ' ' << x[ 5 ] << ' ' << x[ 6 ] << endl;
+    cout << t << ' ' << x[0] << ' ' << x[1] << ' ' << x[2] << ' ' << x[3] << ' ' << x[ 4 ] << ' ' << x[ 5 ] << endl;
+}
+
+void masked( const state_type &x , state_type &dxdt , double t )
+{
+    t = t; 
+    dxdt[0] = - ( ( b * ( 1 - delta ) * ( 1 - epsilon ) * ( alpha * x[2] + x[3] ) ) / ( N - x[4] ) ) * x[0];
+    dxdt[1] = ( ( b * ( 1 - delta ) * ( 1 - epsilon ) * ( alpha * x[2] + x[3] ) ) / ( N - x[4] ) ) * x[0] - sigma * x[1];
+    dxdt[2] = ny * sigma * x[1] - ( theta + iotaA ) * x[2];
+    dxdt[3] = ( 1 - ny ) * sigma * x[1] - ( psi + iotaO + d0 ) * x[3];
+    dxdt[4] = theta * x[2] + psi * x[3] - ( iotaI + d0 ) * x[4];
+    dxdt[5] = iotaI * x[4] + iotaA * x[2] + iotaO * x[3];
+
+    N = x[0] + x[1] + x[2] + x[3] + x[4] + x[5];
+
 }
 
 int main(int argc, char **argv)
 {
     argc = argc;
     argv = argv;
-    //                 S     E    I    A    H    R    D
-    state_type x = { N - 6, 2.0, 3.0, 1.0, 0.0, 1.1, 0.0 }; // initial conditions
-    integrate( masked , x , 0.0 , 200.0 , 0.1 , write_sir );
+    //                S[0]    E[1]    A[2]    I[3]       Id[4]   R[5]
+    state_type x = {   N - 842, 441.0,   188.0, 212.0,     1.0,   0.0 }; // initial conditions
+    integrate( masked , x , 0.0 , 400.0 , 0.1 , write_sir );
+
 }
